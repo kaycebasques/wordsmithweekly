2e6b00257439594603de390768d623addcc442c4
Kayce Basques <kaycebasques@gmail.com>
2017-01-05 17:43:08 -0800

    update theming

--- index.markdown
+++ index.markdown
@@ -1,4 +1,14 @@
 ---
-layout: post
-title: Home
+layout: default
 ---
+<ol class="posts">
+  {% for post in site.posts %}
+    <li class="post">
+      <a class="post__link" href="{{ post.url }}">
+        <p class="post__date">{{ post.date | date: '%B %d, %Y' }}</p>
+        <p class="post__title">{{ post.title}}</p>
+        <p class="post__summary">{{ post.summary }}</p>
+      </a>
+    </li>
+  {% endfor %}
+</ol>
--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
-'use strict';
-
-// Add JS here that you want to run on every page.
+(function () {
+  'use strict';
+  function switchClasses(el, class1, class2) {
+    if (el.classList.contains(class1)) {
+      el.classList.remove(class1);
+      el.classList.add(class2);
+      return;
+    }
+    el.classList.remove(class2);
+    el.classList.add(class1);
+  }
+  function toggleMenu() {
+    switchClasses(document.querySelector('.header'), 'header--visible',
+        'header--hidden');
+    switchClasses(document.querySelector('.nav'), 'nav--visible',
+        'nav--hidden');
+  }
+  document.querySelector('.header__menu').addEventListener('click',
+      toggleMenu);
+  document.querySelector('.nav__close').addEventListener('click',
+      toggleMenu);
+})();
