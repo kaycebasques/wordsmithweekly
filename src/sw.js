7f9e6f935e5bedbc3c9d8c5bd92745850de4e953
Kayce Basques <kaycebasques@gmail.com>
2017-06-19 16:01:14 -0700

    fix workbox declaration

--- src/sw.js
+++ src/sw.js
@@ -1,6 +1,6 @@
 importScripts('workbox-sw.prod.v1.0.1.js');
 
-const workboxSW = new goog.SWLib();
+const workboxSW = new WorkboxSW();
 
 self.addEventListener('fetch', function (event) {
   console.log('requested: ', event.request);
