21269f99ed9b7c4c037ba45c9cde6590d21b8160
Kayce Basques <kaycebasques@gmail.com>
2017-06-23 16:10:49 -0700

    handle pretty URLs

--- src/sw.js
+++ src/sw.js
@@ -2,12 +2,15 @@ importScripts('workbox-sw.prod.v1.0.1.js');
 
 const workbox = new WorkboxSW();
 
-workbox.router.registerRoute(/^[^.]*$/, (args) => {
+function handlePrettyUrl(args) {
   console.log(args);
   if (args.url.pathname.endsWith('/')) return caches.match(args.url +
       'index.html');
   if (args.url.pathname === '') return caches.match('/index.html');
   return caches.match(args.url + '.html');
-});
+}
+
+workbox.router.registerRoute(/^https\:\/\/wordsmithweekly\.com[^.]*$/,
+    handlePrettyUrl);
 
 workbox.precache([]);
